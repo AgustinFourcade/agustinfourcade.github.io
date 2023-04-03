288e570e7496f2d23c15bd854fa595a9e7c4a309
Agus <agustin.fourcade@ingka.ikea.com>
2023-04-03 14:36:29 +0200

    main banner

---
+++ src/components/Banner.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImage from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Backend Developer", "Frontend Developer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100); //How much faster the letters are written
+  const period = 200; //How much time pass between letters
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my portfolio</span>
+            <h1>
+              {"Hi I'm Agustin Fourcade "}
+              <span className="wrap" Fullstack Developer>
+                {text}
+              </span>
+            </h1>
+            <p>Lorem Ipsum is simlpy dummy....</p>
+            <button onClick={() => console.log("Connect")}>
+              Let's connect <ArrowRightCircle size={25}></ArrowRightCircle>
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImage} alt="Headder Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
